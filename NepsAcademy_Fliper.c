94046dc972f2decbc5b110b95eb94963e7f8897f
Eduardo Braga <125268670+Dudubraga@users.noreply.github.com>
2023-04-18 15:20:51 -0300

    Fliper Solution in C

--- NepsAcademy_Fliper.c
+++ NepsAcademy_Fliper.c
@@ -2,14 +2,15 @@
  
 int main() {
 	int p, r;
-    scanf("%d %d", &p, &r);
-    if(p == 0){
-        printf("C\n");
-    }
-    else if(p == 1 && r == 0){
-        printf("B\n");
-    }
-    else if(p == 1 && r == 1){
-        printf("A\n");
-    }
+	scanf("%d %d", &p, &r);
+    	if(p == 0){
+        	printf("C\n");
+    	}
+    	else if(p == 1 && r == 0){
+        	printf("B\n");
+    	}
+    	else if(p == 1 && r == 1){
+        	printf("A\n");
+    	}
+	return 0;
 }
