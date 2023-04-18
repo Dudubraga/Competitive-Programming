55f1074417a3888d0c4f58ff818aa1e1d76533a8
Eduardo Braga <125268670+Dudubraga@users.noreply.github.com>
2023-04-18 15:14:07 -0300

    Challenge of the Greatest Number Solution in C

---
+++ NepsAcademy_MaiorNumero.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+ 
+int main() {
+	int n = 1; 
+    int i;
+    int maior = 0;
+    
+    for(i = 0; n != 0; i++){
+        scanf("%d", &n);
+        if(n > maior){
+            maior = n;
+        }
+    }
+    printf("%d\n", maior);
+
+    return 0;
+}
