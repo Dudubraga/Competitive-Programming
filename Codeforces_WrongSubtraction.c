22314fc9ce294c174bf3a22501a440d955410005
Eduardo Braga <125268670+Dudubraga@users.noreply.github.com>
2023-04-18 16:33:15 -0300

    Wrong Subtraction Solution in C

--- Codeforces_WrongSubtraction.c
+++ Codeforces_WrongSubtraction.c
@@ -2,15 +2,15 @@
  
 int main(){
 	int n, k, i;
-    scanf("%d %d", &n, &k);
-    for(i = 0; i < k; i++){
-        if(n % 10 == 0){
-            n = n / 10;
-        }
-        else{
-            n = n - 1;
-        }
-    }
+	scanf("%d %d", &n, &k);
+    	for(i = 0; i < k; i++){
+        	if(n % 10 == 0){
+			n = n / 10;
+        	}
+        	else{
+            		n = n - 1;
+        	}
+    	}
     printf("%d", n);
 
     return 0;
