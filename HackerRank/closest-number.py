d6ad547de05f7f500834b2b10b53eb19df8de93f
Eduardo Braga <dudubraga2004@gmail.com>
2023-05-14 22:12:43 -0300

    Problem Solved

---
+++ HackerRank/closest-number.py
@@ -0,0 +1,12 @@
+tests = int(input())
+for i in range(tests):
+    a, b, x = map(int, input().split())
+    ab = a ** b
+    abx0 = ab // x
+    abx1 = x * abx0
+    abx2 = x * (abx0 + 1)
+
+    if (ab - abx1) < (abx2 - ab):
+        print(int(abx1))
+    else:
+        print(int(abx2))
---
+++ HackerRank/minimum-height-triangle.py
@@ -0,0 +1,7 @@
+import math
+base, area = map(float, input().split(" "))
+
+h = (area * 2) / base
+result = math.ceil(h)
+
+print(result)
