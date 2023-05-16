e76a1bec1de214cf48cd4e4a1ef566d25bc9af8c
Eduardo Braga <dudubraga2004@gmail.com>
2023-05-16 15:49:30 -0300

    Problem Solved

---
+++ SPOJ (SphereJudgeOnline)/pneu.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int n, m, difference;
+    scanf("%d %d", &n, &m);
+    difference = n - m;
+
+    printf("%d", difference);
+
+    return 0;
+}
---
+++ SPOJ (SphereJudgeOnline)/quadrado.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+	int n, result;
+	scanf("%d", &n);
+	result = n * n;
+
+	printf("%d", result);
+
+	return 0;
+}
