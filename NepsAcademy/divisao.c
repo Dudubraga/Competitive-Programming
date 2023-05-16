45134c274bbe13947b510ce6a63bd1ea06b4508d
Eduardo Braga <dudubraga2004@gmail.com>
2023-05-16 16:06:57 -0300

    Problem Solved

---
+++ NepsAcademy/divisao.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+	float a, b, s;
+	scanf("%f",&a);
+	scanf("%f",&b);
+    s = a / b;
+    printf("%.2f", s);
+
+    return 0;
+}
+
+// Divisão
+// https://neps.academy/br/exercise/85
