3d267f76ed067d96bb7d3b61b87551b7816a1a81
Eduardo Braga <dudubraga2004@gmail.com>
2023-05-19 16:00:52 -0300

    Problem Solved

---
+++ Beecrowd/Python/bee-1022.py
@@ -0,0 +1,34 @@
+tests = int(input())
+for t in range(tests):
+    num1, _, den1, op, num2, _, den2 = input().strip().split()
+    num1 = int(num1)
+    den1 = int(den1)
+    num2 = int(num2)
+    den2 = int(den2)
+
+    if op == '+':
+        den = (den1 * den2)
+        num = (num1 * den2) + (num2 * den1)
+    elif op == '-':
+        den = (den1 * den2)
+        num = (num1 * den2) - (num2 * den1)
+    elif op == '*':
+        den = (den1 * den2)
+        num = (num1 * num2)
+    elif op == '/':
+        den = (den1 * num2)
+        num = (num1 * den2)
+
+    a = num
+    b = den
+    while b != 0:
+        a, b = b, a % b
+    gcd = a
+
+    simp_num = num // gcd
+    simp_den = den // gcd
+
+    print(f'{num}/{den} = {simp_num}/{simp_den}')
+
+# 1022 - TDA Rational
+# https://www.beecrowd.com.br/judge/en/problems/view/1022
