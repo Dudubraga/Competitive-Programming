c6b14611147406ebb45f57abf6f39ea2d74b6ae3
Eduardo Braga <dudubraga2004@gmail.com>
2023-05-21 21:56:46 -0300

    Problem Solved

---
+++ Beecrowd/C/bee-1150.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(){
+
+    int x, z;
+    scanf("%d", &x);
+    while(1){
+        scanf("%d", &z);
+        if(z > x){
+            break;
+        }
+    }
+    int sum = 0, count = 0;
+    for(int i = x; sum <= z; i++){
+        sum += i;
+        count++;
+    }
+    printf("%d\n", count);
+
+    return 0;
+}
+
+// 1150 - Exceeding Z
+// https://www.beecrowd.com.br/judge/en/problems/view/1150
---
+++ Beecrowd/Python/bee-1150.py
@@ -0,0 +1,16 @@
+x = int(input())
+while True:
+    z = int(input())
+    if z > x:
+        break
+sum = 0
+count = 0
+for i in range(x, z+1):
+    if sum > z:
+        break
+    sum += i
+    count +=1
+print(count)
+
+# 1150 - Exceeding Z
+# https://www.beecrowd.com.br/judge/en/problems/view/1150
