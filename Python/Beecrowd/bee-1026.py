9beabd1018f2be9b77b3a95cc72f9fba009a17d5
Eduardo Braga <dudubraga2004@gmail.com>
2023-06-06 12:27:32 -0300

    Problem Solved

---
+++ Python/Beecrowd/bee-1026.py
@@ -0,0 +1,10 @@
+while True:
+    try:
+        a, b = map(int,input().split())
+        print(a ^ b)
+
+    except EOFError:
+        break
+
+# 1026 - Tp Carry or not to Carry
+# https://www.beecrowd.com.br/judge/en/problems/view/1026
