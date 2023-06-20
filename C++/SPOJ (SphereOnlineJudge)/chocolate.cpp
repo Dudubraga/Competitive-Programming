9c7a9fd48a3764195f1bb5bfa4260abc752570bb
Eduardo Braga <dudubraga2004@gmail.com>
2023-06-20 15:36:28 -0300

    Problem Solved

---
+++ C++/SPOJ (SphereOnlineJudge)/chocolate.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int N, M;
+    cin >> N >> M;
+    if(N % (M+1) == 0){
+        cout << "Carlos" << endl;
+    }else{
+        cout << "Paula" << endl;
+    }
+
+    return 0;
+}
+
+/* CHOCPJ09 - Competiçăo de chocolate
+https://br.spoj.com/problems/CHOCPJ09/ */
