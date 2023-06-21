7f0948e12460901f9953c3e90482f6d2133aeca2
Eduardo Braga <dudubraga2004@gmail.com>
2023-06-21 16:40:08 -0300

    Problem Solved

---
+++ C++/CodeForces/fraction.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+bool coPrime(int a, int b){
+    bool coPrime = true;
+    for(int k = 2; k <= a; k++){
+        if(a % k == 0 && b % k == 0){
+            coPrime = false;
+            break;
+        }
+    }
+
+    return coPrime;
+}
+
+int main(){
+    int N, a, b, frac = -1;
+    cin >> N;
+    for(int i = 1; i < (N/2)+1; i++){
+        int j = N-i;
+        if(coPrime(i, j) == true && i / j >= frac){
+            frac = i / j;
+            a = i;
+            b = j;
+        }
+    } 
+    cout << a << " " << b << endl;
+
+    return 0;
+}
+
+/* 854A - Fraction
+https://codeforces.com/contest/854/problem/A */
