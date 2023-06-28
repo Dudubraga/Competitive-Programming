d50f42b6c9c92322635d1db0af3cab9bb7ddba38
Eduardo Braga <dudubraga2004@gmail.com>
2023-06-28 02:27:37 -0300

    Problem Solved

---
+++ C++/SPOJ (SphereOnlineJudge)/selos.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int isPrime(long long n){
+    if(n == 0 || n == 1){
+        return 0;
+    }
+    for(int i = 2; i < sqrt(n); i++){
+        if(n % i == 0){
+            return 1;
+        }
+    }
+    return 0;
+}
+
+int main(){
+    long long N;
+    cin >> N;
+    int possible = isPrime(N);
+    if(possible == 1) cout << "S" << endl;
+    else cout << "N" << endl;
+
+    return 0;
+}
+
+/* SELOS11 - Selos
+https://br.spoj.com/problems/SELOS11/ */
