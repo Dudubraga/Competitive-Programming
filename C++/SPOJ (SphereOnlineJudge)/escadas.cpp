ac8e1df7dff376408b0fd775bf1245b8b0a8e57f
Eduardo Braga <dudubraga2004@gmail.com>
2023-06-28 02:39:58 -0300

    Problem Solved

---
+++ C++/SPOJ (SphereOnlineJudge)/escadas.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int N, seconds = 10;
+    int time[N];
+    cin >> N;
+    for(int i = 0; i < N; i++){
+        cin >> time[i];
+    }
+    for(int j = 1; j < N; j++){
+        if(time[j] < (time[j-1] + 10)){
+            seconds += (time[j] - time[j-1]);
+        }else{
+            seconds += 10;
+        }
+    }
+    cout << seconds << endl;
+    return 0;
+}
+
+/* JESCADA - Escada
+https://br.spoj.com/problems/JESCADA/ */
