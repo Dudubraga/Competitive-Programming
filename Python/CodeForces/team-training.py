e102a0a5d5c82d692579d7cfcd0f18bd3e54b0eb
Eduardo Braga <dudubraga2004@gmail.com>
2023-07-08 00:51:49 -0300

    Problem Solved

---
+++ Python/CodeForces/team-training.py
@@ -0,0 +1,17 @@
+xp, nb = map(int, input().split())
+groups = 0
+
+while xp + nb >= 3 and nb > 0 and xp > 0:
+    if xp > nb:
+        xp -= 2
+        nb -= 1
+        groups += 1
+    else:
+        xp -= 1
+        nb -= 2
+        groups += 1
+
+print(groups)
+
+# 519C - A and B and Team Training
+# https://codeforces.com/problemset/problem/519/C
--- Python/TheHuxley/3n+1.py
+++ Python/TheHuxley/3n+1.py
@@ -1,18 +1,20 @@
+def cycle_length(n):
+    length = 1
+    while n != 1:
+        if n % 2 == 0:
+            n //= 2
+        else:
+            n = n * 3 + 1
+        length += 1
+    return length
+
 while True:
     try:
         i, j = map(int, input().split())
-        max_size = 0
+        max_length = 0
         for n in range(min(i, j), max(i, j) + 1):
-            cycle = 1
-            while n != 1:
-                if n % 2 == 0:
-                    n //= 2
-                else:
-                    n = n * 3 + 1
-                cycle += 1
-            size = cycle
-            max_size = max(size, max_size)
-        print(i, j, max_size)
+            max_length = max(max_length, cycle_length(n))
+        print(i, j, max_length)
     except EOFError:
         break
 
