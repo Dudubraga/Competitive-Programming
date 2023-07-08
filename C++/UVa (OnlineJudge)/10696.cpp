f58edaa0efa3b2ceddd81c5936da06c0f7ce2bd1
Eduardo Braga <dudubraga2004@gmail.com>
2023-07-07 22:53:56 -0300

    Problem Solved

---
+++ C++/UVa (OnlineJudge)/10696.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+int f91(int N){
+    if(N <= 100){
+        return f91(f91(N + 11));
+    }
+    if(N >= 101){
+        return N - 10;
+    }
+    return N;
+}
+
+int main(){
+    int N;
+    while(1){
+        cin >> N;
+        if(N == 0){
+            break;
+        }
+        int x = f91(N);
+        cout << "f91(" << N << ") = " << x << endl;
+    }
+
+    return 0;
+}
+
+/* 10696 f91
+https://onlinejudge.org/external/106/10696.pdf */
