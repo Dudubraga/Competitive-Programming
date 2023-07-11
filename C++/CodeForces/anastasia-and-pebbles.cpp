32a11ed880dd8278fb820a923e1f26d635ea68aa
Eduardo Braga <dudubraga2004@gmail.com>
2023-07-11 17:44:53 -0300

    Problem Solved

---
+++ C++/CodeForces/anastasia-and-pebbles.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main(){
+    double N, K, pebbles, pocket = 0;
+    cin >> N >> K;
+
+    for(int i = 0; i < N; i++){
+        cin >> pebbles;
+        pocket += ceil(pebbles / K);
+    }
+
+    int days = ceil(pocket / 2);
+    cout << days << endl;
+
+    return 0;
+}
+
+/* A. Anastasia and pebbles
+https://codeforces.com/problemset/problem/789/A */
