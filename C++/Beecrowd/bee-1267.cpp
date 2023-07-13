aa4455c22fc4851319cc8cbbb0a20ef758ecb826
Eduardo Braga <dudubraga2004@gmail.com>
2023-07-13 02:18:44 -0300

    Problem Solved

---
+++ C++/Beecrowd/bee-1267.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+    int N, D;
+    while(cin >> N >> D){
+        if(N == 0 && D == 0) break;
+        int attendees[D][N];
+        for(int i = 0; i < D; i++){
+            for(int j = 0; j < N; j++){
+                cin >> attendees[i][j];
+            }
+        }
+        int heigher = 0;
+        for(int i = 0; i < N; i++){
+            int sum = 0;
+            for(int j = 0; j < D; j++){
+                sum += attendees[j][i];
+            }
+            if(sum > heigher){
+                heigher = sum;
+            }
+        }
+        if(heigher == D) cout << "yes" << endl;
+        else cout << "no" << endl;
+    }
+
+    return 0;
+}
+
+/* 1267 - Pascal Library
+https://www.beecrowd.com.br/judge/en/problems/view/1267 */
