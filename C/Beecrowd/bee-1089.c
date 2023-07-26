0f6dd14ffa573f54a8d89b64901248a9e00f21fe
Eduardo Braga <dudubraga2004@gmail.com>
2023-07-26 00:30:49 -0300

    Problem Solved

---
+++ C/Beecrowd/bee-1089.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main(){
+    int N, H, peaks;
+    while(scanf("%d", &N) && N != 0){
+        int wave[N+1];
+        for(int i = 0; i < N; i++){
+            scanf("%d", &H);
+            wave[i] = H;
+        }
+        wave[N] = wave[0];
+        peaks = 0;
+        for(int j = 1; j < N; j++){
+            if(wave[j] > wave[j-1] && wave[j] > wave[j+1]){
+                peaks++;
+            }
+            if(wave[j] < wave[j-1] && wave[j] < wave[j+1]){
+                peaks++;
+            }
+        }
+        if(wave[0] > wave[N-1] && wave[0] > wave[1]){ peaks++; }
+        if(wave[0] < wave[N-1] && wave[0] < wave[1]){ peaks++; }
+        
+        printf("%d\n", peaks);
+    }
+
+    return 0;
+}
+
+/* 1089 - Musical Loop
+https://www.beecrowd.com.br/judge/en/problems/view/1089 */
---
+++ Python/Beecrowd/bee-1089.py
@@ -0,0 +1,23 @@
+while True:
+    N = int(input())
+    if N == 0:
+        break
+    wave = list(map(int, input().split()))
+    wave.append(wave[0])
+
+    peaks = 0
+    for i in range(0, N):
+        if wave[i] > wave[i-1] and wave[i] > wave[i+1]:
+            peaks += 1
+        if wave[i] < wave[i-1] and wave[i] < wave[i+1]:
+            peaks += 1
+
+    if wave[0] > wave[N-1] and wave[0] > wave[1]:
+        peaks += 1
+    if wave[0] < wave[N-1] and wave[0] < wave[1]:
+        peaks += 1
+
+    print(peaks)
+
+# 1089 - Musical Loop
+# https://www.beecrowd.com.br/judge/en/problems/view/1089
