24a0bd11e29bdd1f09ecba08554ee3782cd4db51
Eduardo Braga <dudubraga2004@gmail.com>
2023-08-22 15:40:58 -0300

    Problem Solved

---
+++ C++/Timus/some-words-about-sport.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int N;
+    cin >> N;
+    int monitor[N][N];
+    for(int i = 0; i < N; i++){
+        for(int j = 0; j < N; j++){
+            cin >> monitor[i][j];
+        }
+    }
+    int x = 0, y = 0, z = 0;
+    for(int k = 0; k < N*2-1; k++){
+        if(k < N){ z = k; x += k; y = 0; }
+        else{ z--; x = N-1; y -= z; }
+        cout << monitor[x][y] << " ";
+        for(int l = 0; l < z; l++){
+            x--; y++;
+            cout << monitor[x][y] << " ";
+        }
+    } cout << endl;
+
+    return 0;
+}
+
+/* 1313 - Some Words about Sport
+https://acm.timus.ru/problem.aspx?space=1&num=1313 */
