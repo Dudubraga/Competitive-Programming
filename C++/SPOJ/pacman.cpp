0256b4fcdc5032c0bc2b889305fff88dcdab7ab5
Eduardo Braga <dudubraga2004@gmail.com>
2023-09-05 15:44:47 -0300

    Problem Solved

---
+++ C++/SPOJ/pacman.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int N;
+    cin >> N;
+    char game[N][N];
+    for(int i = 0; i < N; i++){
+        for(int j = 0; j < N; j++){
+            cin >> game[i][j];
+        }
+    }
+    int move = 0, dir = 1, count = 0, max = 0;
+    for(int i = 0; i < N; i++){
+        for(int j = 0; j < N; j++){
+            if(game[i][move] == 'o'){ count++; }
+            else if(game[i][move] == 'A'){ count = 0; }
+            if(count > max){ max = count; }
+            move += dir;
+        }
+        if(move == N){ dir = -1; move--; }
+        else{ dir = 1; move++; }
+    }
+    cout << max << endl;
+
+    return 0;
+}
+
+/* PACMAN14 - PacMan
+https://br.spoj.com/problems/PACMAN14/ */
