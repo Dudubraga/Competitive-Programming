e95e0b1b47ace0dbc2a7ede51eca9555eaed8bad
Eduardo Braga <dudubraga2004@gmail.com>
2023-09-13 20:08:25 -0300

    Problem Solved

---
+++ C++/SPOJ/eleicoes.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <map>
+#include <iterator>
+using namespace std;
+
+int main(){
+    int N, x;
+    map<int, int>votes;
+    cin >> N;
+    for(int i = 0; i < N; i++){
+        cin >> x;
+        votes[x] += 1;
+    }
+    map<int, int>::iterator itr;
+    int max = 0, winner;
+    for(itr = votes.begin(); itr != votes.end(); itr++){
+        if(itr->second > max){
+            max = itr->second;
+            winner = itr->first;
+        }
+    }
+    cout << winner << endl;
+
+    return 0;
+}
+
+/* ELEICOES - Eleições
+https://br.spoj.com/problems/ELEICOES/ */
