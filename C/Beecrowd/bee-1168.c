90a1940ee64ccdcc14fb7108c0c52e97c89f30c2
Eduardo Braga <dudubraga2004@gmail.com>
2023-09-14 22:39:13 -0300

    Problem Solved

---
+++ C/Beecrowd/bee-1168.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+    int N, leds[10] = {6,2,5,5,4,5,6,3,7,6};
+    scanf("%d\n", &N);
+    char num[101];
+    for(int i = 0; i < N; i++){
+        scanf("%s", &num);
+        int size = strlen(num), ans = 0;
+        for(int j = 0; j < size; j++){
+            ans += leds[num[j] - '0'];
+        }
+        printf("%d leds\n", ans);
+    }
+
+    return 0;
+}
+
+/* 1168 - LED
+https://www.beecrowd.com.br/judge/en/problems/view/1168 */
