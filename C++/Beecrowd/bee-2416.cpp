d65629f2d0916cc458dedf3c88fa808d20c8fc97
Eduardo Braga <dudubraga2004@gmail.com>
2023-09-14 23:42:35 -0300

    Problem Solved

---
+++ C++/Beecrowd/bee-2416.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int C, N;
+    cin >> C >> N;
+    cout << C % N << endl;
+
+    return 0;
+}
+
+/* 2416 - Corrida
+https://www.beecrowd.com.br/judge/en/problems/view/2416 */
---
+++ C/Beecrowd/bee-2416.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    int C, N;
+    scanf("%d %d", &C, &N);
+    printf("%d\n", C % N);
+    
+    return 0;
+}
+
+/* 2416 - Corrida
+https://www.beecrowd.com.br/judge/en/problems/view/2416 */
