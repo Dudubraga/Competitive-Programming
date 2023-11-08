68709a75fe8227cd3e9ff17ea67058d0f459bee1
Eduardo Braga <dudubraga2004@gmail.com>
2023-11-08 19:34:49 -0300

    Problem Solved

--- C/UVa Online Judge/696.c
+++ C/UVa Online Judge/696.c
@@ -1,48 +1,42 @@
 #include <stdio.h>
 
 int HowManyKnights(int linhas, int colunas){
-    int n, tabuleiro, cavalos;
-    tabuleiro = colunas * linhas;
-    /* CASO 1 */
-    if(linhas == 1 || colunas == 1){
-        cavalos = colunas * linhas;
-    }
-    /* CASO 2 */
-    else if(linhas == 2 || colunas == 2){
-        if(linhas > 2){ n = linhas; }
-        else{ n = colunas; }
-        cavalos = 4;
-        for(int i = 2; i < n; i++){
-            if(i % 4 == 0 || (i-1) % 4 == 0){
-                cavalos += 2;
-            }
-        }
-    }
-    /* CASO 3 */
-    else{
-        if(tabuleiro % 2 == 0){
-            cavalos = tabuleiro / 2;
-        }else{
-            cavalos = (tabuleiro + 1) / 2;
-        }
-    }
-    return cavalos;
+	int cavalos, tabuleiro;
+	tabuleiro = linhas * colunas;
+	/* CASO 1 */
+	if(linhas >= 3 && colunas >= 3){
+		if(tabuleiro % 2 == 0){ cavalos = tabuleiro / 2; }
+		else{ cavalos = (tabuleiro + 1) / 2; }
+	}
+        /* CASO 2 */
+        else if(linhas == 1 || colunas == 1){ cavalos = tabuleiro; }
+	/* CASO 3 */
+	else if(linhas == 2 || colunas == 2){
+		int n;
+		if(linhas == 2){ n = colunas; }
+		else{ n = linhas; }				
+	
+		if(n % 4 == 0){ cavalos = n; }
+		else if(n % 2 != 0){ cavalos = n + 1; }
+		else{ cavalos = n + 2; }
+	}
+
+	return cavalos;
 }
 
 int main(){
-    int linhas, colunas, cavalos;
-    while(1){
-        scanf("%d %d", &linhas, &colunas);
-        if(colunas == 0 && linhas == 0){ break; }
+	int cavalos, linhas, colunas;
+	while(1){
+		scanf("%d %d", &linhas, &colunas);
+		if(linhas == 0 || colunas == 0){ break; }
 
-        cavalos = HowManyKnights(linhas, colunas);
+		cavalos = HowManyKnights(linhas, colunas);
 
-        printf("%d knights may be placed on a %d row %d column board.\n", cavalos, linhas, colunas);
-    }
+		printf("%d knights may be placed on a %d row %d column board.\n", cavalos, linhas, colunas);
+	}
 
-    return 0;
+	return 0;
 }
 
 /* 696 - How Many Knights
-https://onlinejudge.org/external/6/696.pdf */
-
+* https://onlinejudge.org/external/6/696.pdf */
