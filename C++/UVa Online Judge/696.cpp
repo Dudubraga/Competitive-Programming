5fbcf8efe649c9eafd970a56a91b594d052bc32d
Eduardo Braga <dudubraga2004@gmail.com>
2023-11-08 19:43:18 -0300

    Problem Solved

--- C++/UVa Online Judge/696.cpp
+++ C++/UVa Online Judge/696.cpp
@@ -5,42 +5,23 @@ int main(){
     int columns, rows, knights, board, x;
     while(true){
         cin >> rows >> columns;
-        if(columns == 0 || rows == 0){ 
-            break;
-        }
-        board = columns * rows;
-        if(columns == 1 || rows == 1){
-            knights = columns * rows;
+        if(columns == 0 || rows == 0){ break; }
+        
+	board = columns * rows;
+        if(columns == 1 || rows == 1){ knights = columns * rows; }
+        else if(columns == 2 || rows == 2){
+       		if(columns >= rows){ x = columns; }
+		else{ x = rows; }
+		knights = 4;
+		for(int j = 3; j < x; j++){
+			if(j % 4 == 0 || j % 4 == 1){
+    				knights += 2;
+                    	}
+                }
         }else{
-            if(columns == 2 || rows == 2){
-                if(columns >= rows){
-                    x = columns;
-                }else{
-                    x = rows;
-                }
-                if(x <= 3){
-                    for(int i = 0; i < 3; i++){
-                        knights = 4;
-                    }
-                }else{
-                    for(int i = 0; i < 3; i++){
-                        knights = 4;
-                    }
-                    for(int j = 3; j < x; j++){
-                        if(j % 4 == 0 || j % 4 == 1){
-                            knights += 2;
-                        }else{
-                            knights = knights; 
-                        }
-                    }
-                }
-            }
-            else if(board % 2 == 0){
-                knights = board / 2;
-            }else{
-                knights = (board + 1) / 2;
-            }
-        }
+            if(board % 2 == 0){ knights = board / 2; }
+	    else{ knights = (board + 1) / 2; }
+	}
         cout << knights << " knights may be placed on a " << rows << " row " << columns << " column board.\n";
     }
  
