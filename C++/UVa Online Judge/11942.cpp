768d563c7f639a213d53d4c43ad4e9067a6f2bd0
Eduardo Braga <dudubraga2004@gmail.com>
2023-11-25 11:18:41 -0300

    Problem Solved

---
+++ C++/UVa Online Judge/11942.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int groups; cin >> groups;
+    cout << "Lumberjacks:" << endl;
+    while(groups--){
+        int beards[10];
+        for(int i = 0; i < 10; i++){ cin >> beards[i]; }
+        int cre = 1;
+        for(int i = 0; i < 9; i++){
+            if(beards[i] < beards[i+1]){ cre++; }
+        }
+        int decre = 1;
+        for(int i = 0; i < 9; i++){
+            if(beards[i] > beards[i+1]){ decre++; }
+        }
+        if(cre == 10 || decre == 10){
+            cout << "Ordered" << endl;
+        }else{
+            cout << "Unordered" << endl;
+        }
+    }
+
+    return 0;
+}
+
+/* 11942 - Lumberjack Sequencing
+https://onlinejudge.org/external/119/11942.pdf */
