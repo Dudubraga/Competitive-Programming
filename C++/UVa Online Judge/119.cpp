101bd81f06bf43456ba33fafb60eb3f6964edd60
Eduardo Braga <dudubraga2004@gmail.com>
2023-12-15 08:24:51 -0300

    Problem Solved

--- C++/UVa Online Judge/119.cpp
+++ C++/UVa Online Judge/119.cpp
@@ -1,38 +1,52 @@
+
 #include <iostream>
 #include <vector>
 #include <string>
 using namespace std;
 
 int main(){
-    int N;
-    vector<string>names;
-    vector<int>gifts;
-    string name;
+    int N, cont = 0;
+    vector<string>nomes;
+    vector<int>valores;
+    string nome;
     while(cin >> N){
-        int cash, people, pos1, pos2;
+        if(cont != 0){
+            cout << endl;
+        }
+        int valor, pessoas, pos1, pos2;
         for(int i = 0; i < N; i++){
-            cin >> name;
-            names.push_back(name);
-            gifts.push_back(0);
+            cin >> nome;
+            nomes.push_back(nome);
+            valores.push_back(0);
         }
         for(int j = 0; j < N; j++){
-            cin >> name >> cash >> people;
+            cin >> nome >> valor >> pessoas;
             for(int k = 0; k < N; k++){
-                if(name == names[k]){ pos1 = k; }
-            }
-            gifts[pos1] -= cash;
-            for(int l = 0; l < people; l++){
-                cin >> name;
-                for(int m = 0; m < N; m++){
-                    if(name == names[m]){ pos2 = m; }
+                if(nome == nomes[k]){
+                    pos1 = k;
                 }
-                gifts[pos2] += cash / people;
             }
-            gifts[pos1] += cash % people;
+            if(pessoas != 0){
+                valores[pos1] -= valor;
+                for(int l = 0; l < pessoas; l++){
+                    cin >> nome;
+                    for(int m = 0; m < N; m++){
+                        if(nome == nomes[m]){
+                            pos2 = m;
+                        }
+                    }
+                    valores[pos2] += valor / pessoas;
+                }
+                valores[pos1] += valor % pessoas;
+            }
         }
         for(int i = 0; i < N; i++){
-            cout << gifts[i] << " ";
-        } cout << endl;
+            cout << nomes[i] << " " << valores[i] << endl;
+        }
+
+        nomes.clear();
+        valores.clear();
+        cont++;
     }
 
     return 0;
