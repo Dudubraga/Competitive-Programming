2dd46d5897af82a1903f388cd5b388947495472b
Eduardo Braga <dudubraga2004@gmail.com>
2024-02-27 15:38:02 -0300

    Problem Solved

---
+++ C++/NepsAcademy/dentista.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <utility>
+#include <algorithm>
+using namespace std;
+
+int main(){
+    vector<pair<int, int>>times;
+    int n; cin >> n;
+    for(int i = 0; i < n; i++){
+        int x, y; cin >> x >> y;
+        times.push_back(make_pair(y, x));
+    }
+    sort(times.begin(), times.end());
+    int count = 1, j = 0;
+    for(int i = 1; i < n; i++){
+        if(times[i].second >= times[j].first){
+            count++; j = i;
+        }
+    }
+    cout << count << endl;
+
+    return 0;
+}
+
+/* Dentista
+https://neps.academy/br/exercise/248 */
---
+++ C++/Timus/stone-pile.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int w[21], total = 0, best = 100001;
+
+void pile(int n, int wei, int i){
+    if(i == n){ return; }
+    if(abs((total-wei) - wei) < best){ best = abs((total-wei) - wei); }
+    pile(n, wei, i+1);
+    pile(n, wei += w[i], i+1);
+}
+
+int main(){
+    int n; cin >> n;
+    for(int i = 0; i < n; i++){
+        cin >> w[i];
+        total += w[i];
+    }
+    pile(n, 0, 0);
+    cout << best << endl;
+
+    return 0;
+}
+
+/* 1005 - Stone Pile
+https://acm.timus.ru/problem.aspx?space=1&num=1005 */
