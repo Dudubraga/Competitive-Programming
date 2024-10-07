a10fd6fa0c83de73fed739e9030c0f21f6e6d1be
Eduardo Braga <dudubraga2004@gmail.com>
2024-10-07 15:30:20 -0300

    Problem Solved

---
+++ C++/Beecrowd/bee-1066.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a = 0, b = 0, c = 0, d = 0;
+    int n;
+    for(int i = 0; i < 5; i++){
+        cin >> n;
+        if(n % 2 == 0){ 
+            a++; 
+        }
+        if(n % 2 != 0){ 
+            b++; 
+        }
+        if(n > 0){
+            c++;
+        }
+        if(n < 0){
+            d++;
+        }
+    }
+    cout << a << " valor(es) par(es)" << endl;
+    cout << b << " valor(es) impar(es)" << endl;
+    cout << c << " valor(es) positivo(s)" << endl;
+    cout << d << " valor(es) negativo(s)" << endl;
+ 
+    return 0;
+}
+
+/* 1066 - Even, Odd, Positive and Negative
+https://judge.beecrowd.com/en/problems/view/1066 */
