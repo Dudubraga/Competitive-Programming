644e506cc9bd789ba779904d940ea77bb5fc731f
Dudubraga <dudubraga2004@gmail.com>
2025-03-16 20:21:35 -0300

    Problem Solved

---
+++ C++/Beecrowd/bee-1255.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t; cin >> t;
+    cin.ignore();
+    while(t--){
+        string msg;
+        getline(cin, msg);
+        int freq[26] = {0};
+
+        for(int i = 0; i < msg.length(); i++){
+            if(msg[i] >= 'a' && msg[i] <= 'z'){
+                freq[msg[i] - 'a']++;
+            }
+            if(msg[i] >= 'A' && msg[i] <= 'Z'){
+                freq[msg[i] - 'A']++;
+            }
+        }
+
+        int max = 0;
+        for(int i = 0; i < 26; i++){
+            if(freq[i] > max){
+                max = freq[i];
+            }
+        }
+        for(int i = 0; i < 26; i++){
+            if(freq[i] == max){
+                printf("%c", i + 'a');
+            }
+        } cout << endl;    
+    }
+    
+    return 0;
+}
+
+/* 1255 - Letter Frequency
+https://judge.beecrowd.com/en/problems/view/1255 */
