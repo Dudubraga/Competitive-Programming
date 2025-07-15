24519d62780c9d1b26c47341054a45035a8e7216
Dudubraga <dudubraga2004@gmail.com>
2025-07-15 09:09:22 -0300

    MFP 2025 - First Phase

---
+++ Codeforces/reverse-engineering/LINK.md
@@ -0,0 +1,1 @@
+[Reverse Engineering](https://codeforces.com/group/9CNwiex6Ir/contest/606592/problem/E)
---
+++ Codeforces/reverse-engineering/reverse-engineering.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define _ ios::sync_with_stdio(0);cin.tie(0); 
+#define endl '\n'
+#define all(x) x.begin(), x.end()
+#define pb push_back
+#define mp make_pair
+
+int main(){ _
+    int n, m, a; cin >> n >> m;
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < m; j++){
+            cin >> a;
+
+            if(i%2==0 && j%2==0 && a%2==1){a++;} 
+            if(i%2==1 && j%2==1 && a%2==1){a++;}
+
+            if(i%2==0 && j%2==1 && a%2==0){a++;} 
+            if(i%2==1 && j%2==0 && a%2==0){a++;}
+
+            cout << a << " ";
+        } cout << endl;
+    }
+
+    return 0;
+}
